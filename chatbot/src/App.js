cbf522e8574557d2a57f6e888fc64a71991725d6
Quoc Duong Nguyen <quoc-duong.nguyen@epita.fr>
2023-03-14 19:40:54 +0100

    Add components for chatbot

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,52 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-import { useWhisper } from '@chengsokdara/use-whisper'
-
-const Whisper = () => {
-  const {
-    recording,
-    speaking,
-    transcribing,
-    transcript,
-    pauseRecording,
-    startRecording,
-    stopRecording,
-  } = useWhisper({
-    apiKey: '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', // YOUR_OPEN_AI_TOKEN
-  })
-
-  return (
-    <div>
-      <p>Recording: {recording}</p>
-      <p>Speaking: {speaking}</p>
-      <p>Transcribing: {transcribing}</p>
-      <p>Transcribed Text: {transcript.text}</p>
-      <button onClick={() => startRecording()}>Start</button>
-      <button onClick={() => pauseRecording()}>Pause</button>
-      <button onClick={() => stopRecording()}>Stop</button>
-    </div>
-  )
-}
+import ChatBot from 'react-simple-chatbot';
+import Socket from './Components/Socket';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Whisper />
-    </div>
+    <ChatBot
+      headerTitle="Come talk to me please 🥺"
+      steps={[
+        {
+          id: '1',
+          message: 'Hey there, I am the best SMC chatbot ever made, what can I do for you?',
+          trigger: 'search'
+        },
+        {
+          id: 'search',
+          user: true,
+          trigger: '3'
+        },
+        {
+          id: '3',
+          component: <Socket />,
+          asMessage: true,
+          waitAction: true,
+          trigger: '1',
+        }
+      ]}
+    />
   );
 }
 
---
+++ chatbot/src/Components/Socket.js
@@ -0,0 +1,117 @@
+import { Component } from 'react';
+import { Loading } from 'react-simple-chatbot'
+import PropTypes from 'prop-types';
+import io from 'socket.io-client';
+
+class Socket extends Component {
+    constructor(props) {
+        super(props);
+
+        this.socket = io('http://localhost:5005')
+
+        this.state = {
+            loading: true,
+            result: '',
+            trigger: false,
+        };
+
+        const self = this
+        self.socket.on('connect', function () {
+            console.log('Connected to Socket.io server.');
+            self.socket.emit('session_request', {
+                'session_id': self.getSessionId(),
+            });
+            console.log(`Session ID: ${self.getSessionId()}`);
+        });
+
+        self.socket.on('connect_error', (error) => {
+            // Write any connection errors to the console
+            console.error(error);
+            self.setState({ loading: false, result: "Sorry I'm too tired to respond, check back later 😴" })
+        });
+
+        this.triggerNext = this.triggerNext.bind(this);
+    }
+
+    componentWillMount() {
+        const { steps } = this.props;
+        const search = steps.search.value;
+        const self = this
+
+        this.utter(search)
+
+        console.log('wtf')
+
+        self.socket.on('bot_uttered', function (response) {
+            console.log('Bot uttered:', response);
+            if (response.text) {
+                self.setState({ loading: false, result: response.text })
+            }
+        });
+    }
+
+    getSessionId = () => {
+        const storage = localStorage;
+        const storageKey = 'RASA_SESSION_ID';
+        const savedId = storage.getItem(storageKey);
+        if (savedId) {
+            return savedId;
+        }
+        const newId = this.socket.id;
+        storage.setItem(storageKey, newId);
+        return newId;
+    }
+
+    utter = (msg) => {
+        this.socket.emit('user_uttered', {
+            'message': msg,
+            'session_id': this.getSessionId(),
+        });
+    }
+
+    triggerNext() {
+        this.setState({ trigger: true }, () => {
+            this.props.triggerNextStep();
+        });
+    }
+
+    render() {
+        const { trigger, loading, result } = this.state;
+
+        return (
+            <div className="dbpedia">
+                {loading ? <Loading /> : result}
+                {
+                    !loading &&
+                    <div
+                        style={{
+                            textAlign: 'center',
+                            marginTop: 20,
+                        }}
+                    >
+                        {
+                            !trigger &&
+                            <button
+                                onClick={() => this.triggerNext()}
+                            >
+                                Search Again
+                            </button>
+                        }
+                    </div>
+                }
+            </div>
+        );
+    }
+}
+
+Socket.propTypes = {
+    steps: PropTypes.object,
+    triggerNextStep: PropTypes.func,
+};
+
+Socket.defaultProps = {
+    steps: undefined,
+    triggerNextStep: undefined,
+};
+
+export default Socket
