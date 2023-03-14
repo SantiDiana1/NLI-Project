1d4e59f01fba8771ca25c6e2b68529ef3c7f9ed6
Quoc Duong Nguyen <quoc-duong.nguyen@epita.fr>
2023-03-14 17:05:22 +0100

    Add inital react-app

---
+++ chatbot/src/App.js
@@ -0,0 +1,53 @@
+import logo from './logo.svg';
+import './App.css';
+import { useWhisper } from '@chengsokdara/use-whisper'
+
+const Whisper = () => {
+  const {
+    recording,
+    speaking,
+    transcribing,
+    transcript,
+    pauseRecording,
+    startRecording,
+    stopRecording,
+  } = useWhisper({
+    apiKey: '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', // YOUR_OPEN_AI_TOKEN
+  })
+
+  return (
+    <div>
+      <p>Recording: {recording}</p>
+      <p>Speaking: {speaking}</p>
+      <p>Transcribing: {transcribing}</p>
+      <p>Transcribed Text: {transcript.text}</p>
+      <button onClick={() => startRecording()}>Start</button>
+      <button onClick={() => pauseRecording()}>Pause</button>
+      <button onClick={() => stopRecording()}>Stop</button>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Whisper />
+    </div>
+  );
+}
+
+export default App;
